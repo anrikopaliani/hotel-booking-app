7be937677618309f39cf38a7fdd5cd613b368c91
anrikopaliani <anrikopa@gmail.com>
2024-03-29 00:42:16 +0400

    refactor: routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>homepage</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
