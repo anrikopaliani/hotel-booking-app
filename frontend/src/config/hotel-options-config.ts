9259ef2399052957e95146e1a129a077cc46b655
anrikopaliani <anrikopa@gmail.com>
2024-04-13 14:43:51 +0400

    feat: type section form

--- frontend/src/config/hotel-options-config.ts
+++ frontend/src/config/hotel-options-config.ts
@@ -1,1 +1,17 @@
-export const hotelTypes = [];
+export const hotelTypes = [
+  "Budget",
+  "Boutique",
+  "Luxury",
+  "Ski Resort",
+  "Business",
+  "Family",
+  "Romantic",
+  "Hiking Resort",
+  "Cabin",
+  "Beach Resort",
+  "Golf Resort",
+  "Motel",
+  "All Inclusive",
+  "Pet Friendly",
+  "Self Catering",
+];
