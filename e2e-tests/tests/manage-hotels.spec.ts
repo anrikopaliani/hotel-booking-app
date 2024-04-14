0e99f02264170ff2eb5fc0168b7e053459e5ae60
anrikopaliani <anrikopa@gmail.com>
2024-04-14 19:21:13 +0400

    test:allow user to add hotel

---
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "playwright/test";
+import path from "path";
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await expect(page.getByRole("heading", { name: "Sign in" })).toBeVisible();
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password123");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+});
+
+test("should allow user to add hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}add-hotel`);
+  await page.locator('[name="name"]').fill("Test Hotel");
+  await page.locator('[name="city"]').fill("test city");
+  await page.locator('[name="country"]').fill("test country");
+  await page
+    .locator('[name="description"]')
+    .fill("description for the test hotel");
+  await page.locator('[name="pricePerNight"]').fill("100");
+  await page.selectOption('select[name="starRating"]', "3");
+  await page.getByText("Budget").click();
+
+  await page.getByLabel("Free Wifi").check();
+  await page.getByLabel("Parking").check();
+
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("4");
+
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "1.jpg"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
