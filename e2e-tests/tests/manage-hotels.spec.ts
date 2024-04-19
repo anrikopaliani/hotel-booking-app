5346e28692ac6c9d56624ff3f4545a954217a5e7
anrikopaliani <anrikopa@gmail.com>
2024-04-20 00:53:35 +0400

    test: should edit hotel

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -57,3 +57,23 @@ test("should display hotels", async ({ page }) => {
   await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
   await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+  await page.getByRole("link", { name: "View Details" }).click();
+
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Dublin Getaways updated");
+  await page.getByRole("button", { name: "Save" }).click();
+
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Dublin Getaways updated"
+  );
+  await page.locator('[name="name"]').fill("Dubling Getaways");
+  await page.getByRole("button", { name: "Save" }).click();
+});
