3a11b7df30c63413f71bd6336f032c881abcc867
anrikopaliani <anrikopa@gmail.com>
2024-04-21 23:56:08 +0400

    feat: get individual hotel

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,7 +1,29 @@
 import express, { Request, Response } from "express";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
+import { param, validationResult } from "express-validator";
 const router = express.Router();
+
+router.get(
+  "/:id",
+  [param("id").notEmpty().withMessage("Hotel ID is required")],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const id = req.params.id.toString();
+
+    try {
+      const hotel = await Hotel.findById(id);
+      res.json(hotel);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
 
 router.get("/search", async (req: Request, res: Response) => {
   try {
