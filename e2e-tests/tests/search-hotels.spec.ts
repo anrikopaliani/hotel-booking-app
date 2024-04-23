4ee66815697cbd132d858cd935efe85f4b4e087b
anrikopaliani <anrikopa@gmail.com>
2024-04-24 01:16:29 +0400

    test: should show hotel detail

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -24,3 +24,14 @@ test("should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Dublin")).toBeVisible();
   await expect(page.getByText("Dubling Getaways")).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+  await page.getByPlaceholder("Where are you going?").fill("Dublin");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Dubling Getaways").click();
+  await expect(page).toHaveURL(/detail/);
+
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
