9cb8a605c620864af5ff99862ffe6fccffc9c8d4
anrikopaliani <anrikopa@gmail.com>
2024-04-25 01:44:19 +0400

    feat: route for booking

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -13,7 +13,21 @@ export interface HotelType {
   starRating: number;
   imageUrls: string[];
   lastUpdated: Date;
+  bookings: BookingType[];
 }
+
+export type BookingType = {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalCost: number;
+};
 
 export type HotelSearchResponse = {
   data: HotelType[];
