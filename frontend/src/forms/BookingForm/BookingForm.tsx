f594abf3d05471fd034bf3eaa047f9324833e90c
anrikopaliani <anrikopa@gmail.com>
2024-04-26 00:26:58 +0400

    feat: booking hotel

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -5,26 +5,59 @@ import {
 } from "../../../../backend/src/shared/types";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { StripeCardElement } from "@stripe/stripe-js";
+import { useSearchContext } from "../../context/SearchContext";
+import { useParams } from "react-router-dom";
+import { useMutation } from "react-query";
+import * as apiClient from "../../api-client";
+import { useAppContext } from "../../context/AppContext";
 
 type Props = {
   currentUser: UserType;
   paymentIntent: PaymentIntentResponse;
 };
 
-type BookingFormData = {
+export type BookingFormData = {
   firstName: string;
   lastName: string;
   email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: string;
+  checkOut: string;
+  hotelId: string;
+  paymentIntentId: string;
+  totalCost: number;
 };
 
 const BookingForm = ({ currentUser, paymentIntent }: Props) => {
   const stripe = useStripe();
   const elements = useElements();
+  const search = useSearchContext();
+  const { hotelId } = useParams();
+
+  const { showToast } = useAppContext();
+
+  const { mutate: bookRoom, isLoading } = useMutation(apiClient.createBooking, {
+    onSuccess: () => {
+      showToast({ message: "Booking Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error saving booking!", type: "ERROR" });
+    },
+  });
+
   const { handleSubmit, register } = useForm<BookingFormData>({
     defaultValues: {
       firstName: currentUser.firstName,
       lastName: currentUser.lastName,
       email: currentUser.email,
+      adultCount: search.adultCount,
+      childCount: search.childCount,
+      checkIn: search.checkIn.toISOString(),
+      checkOut: search.checkOut.toISOString(),
+      hotelId: hotelId,
+      totalCost: paymentIntent.totalCost,
+      paymentIntentId: paymentIntent.paymentIntentId,
     },
   });
 
@@ -39,12 +72,15 @@ const BookingForm = ({ currentUser, paymentIntent }: Props) => {
     });
 
     if (result.paymentIntent?.status === "succeeded") {
-      // dsd
+      bookRoom({ ...formData, paymentIntentId: result.paymentIntent.id });
     }
   };
 
   return (
-    <form className="grid grid-cols-1 gap-5 rounded-lg border p-5 border-slate-300">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="grid grid-cols-1 gap-5 rounded-lg border p-5 border-slate-300"
+    >
       <span className="text-3xl font-bold">Confirm Your Details</span>
       <div className="grid grid-cols-2 gap-6">
         <label className="text-gray-700 text-sm font-bold flex-1">
@@ -94,6 +130,15 @@ const BookingForm = ({ currentUser, paymentIntent }: Props) => {
           id="payment-element"
           className="border rounded-md p-2 text-sm"
         />
+      </div>
+      <div className="flex justify-end">
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="bg-blue-600 text-white font-bold p-2 hover:bg-blue-500 text-md disabled:bg-gray-500"
+        >
+          {isLoading ? "Saving..." : "Confirm Booking"}
+        </button>
       </div>
     </form>
   );
